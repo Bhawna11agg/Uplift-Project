5b3df84d6a1fc0f67da1cfce83bfafa38abc263c
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-07-10 18:54:52 +0530

    Create Top_View_on_Tree.cpp

---
+++ MohitRadadiya/July/Top_View_on_Tree.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+    void print_left(Node * root){
+        if(root == NULL)
+        {    
+            return;
+        }
+        print_left(root->left);
+        cout << root->data << " ";
+    }
+    void topView(Node * root) {
+        print_left(root);
+        Node * temp=root->right;
+        while(temp != NULL){
+            cout << temp->data << " ";
+            temp=temp->right;
+        }
+    }
+    
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.topView(root);
+    return 0;
+}
