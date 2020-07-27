fca16cd93db07c73dd79d0acbb663d5fd9963ee1
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-07-27 19:32:20 +0530

    Update Guessing_game.cpp

--- Projects/Guessing_game.cpp
+++ Projects/Guessing_game.cpp
@@ -20,3 +20,31 @@ int main()
   	}while (guess != compGuess);
 	return 0;
 }
+
+
+/*
+OUTPUT:
+
+Please pick a number between 1 - 1500!
+Enter your estimate: 1300
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 650
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 325
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 162
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 81
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 40
+Your Guess number is less than the Computer Guess number.
+Enter your estimate: 62
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 51
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 45
+Your Guess number is greter than the Computer Guess number.
+Enter your estimate: 42
+Hureyy! Your guess is right!
+
+*/
